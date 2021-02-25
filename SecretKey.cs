78f99b89efe4e19b12c2e96d38f087d29aa1f05b
ericremneback <79632735+ericremneback@users.noreply.github.com>
2021-02-25 12:24:02 +0100

    Add files via upload

---
+++ SecretKey.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace PasswordManager
+{
+    class SecretKey
+    {
+
+        public SecretKey()
+        {
+
+        }
+
+        
+
+        public static byte[] sKeyGenerator()
+        {
+            byte[] sKey = new byte[16];
+            using (RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider())
+            {
+                rngCsp.GetBytes(sKey);
+                return sKey;
+            }
+            
+        }
+
+
+
+    }
+}
