8a09bcd5c8e590b4022ac35e1643f5b4d6eb6c3d
SimonLindholm98 <65282481+SimonLindholm98@users.noreply.github.com>
2021-03-04 16:28:37 +0100

    Update FileConnectServer.cs

--- FileConnectServer.cs
+++ FileConnectServer.cs
@@ -4,7 +4,7 @@ using System.IO;
 using System.Security.Cryptography;
 using System.Collections.Generic;
 
-namespace Code_off
+namespace PasswordManager
 {
     public class FileConnectServer : FileConnector
     {
@@ -15,8 +15,9 @@ namespace Code_off
             return svar;
         }
 
-        public static void CreateAccount()
+        public static void CreateAccount(string mpwd)
         {
+            
             byte[] openIv;
             WriteToFile(); //skapar en secret key
 
@@ -25,51 +26,56 @@ namespace Code_off
                 openIv = myAes.IV;
             }
 
+            Dictionary<string, string> Emptydick = new Dictionary<string, string>();
+            string jsonString3 = JsonSerializer.Serialize(Emptydick);
+            byte[] y = EncryptStringToBytes_Aes(jsonString3, ConnectsKeyAndPsw(mpwd), openIv);
+            
+
             serverFile serverObj1 = new serverFile()
             {
-                vault = null,
+                vault = y,
                 IV = openIv //skapa en IV genom aes
             };
 
             string jsonString1 = JsonSerializer.Serialize(serverObj1);
-            File.WriteAllText(@"ServerInfo2.json", jsonString1);
+            File.WriteAllText(@"IV.json", jsonString1);
         }
 
-        public static void writeToServer(string inputPsw, string txt, string value) 
+        public static void writeToServer(string inputPsw, string txt, string value)
         {
             byte[] openIv;
             //byte[] openEnc;
             byte[] svar4;
-           
+
             using (Aes myAes = Aes.Create())
             {
-                
+
                 byte[] secretKey = ConnectsKeyAndPsw(inputPsw);
                 openIv = readFromServerIv();
                 //openEnc = EncryptStringToBytes_Aes(txt, secretKey, openIv);
                 //gogo.Add(txt, value);
                 //serialisera och sen kryptera gogo
-                
+
                 string svar = DecryptStringFromBytes_Aes(readFromServerVault(), ConnectsKeyAndPsw(inputPsw), readFromServerIv());
-                Dictionary<string,string> readResult = JsonSerializer.Deserialize<Dictionary<string, string>>(svar);
+                Dictionary<string, string> readResult = JsonSerializer.Deserialize<Dictionary<string, string>>(svar);
                 string jsonString3 = JsonSerializer.Serialize(Vault.AddToVault(txt, value, readResult));
                 svar4 = EncryptStringToBytes_Aes(jsonString3, secretKey, openIv);
-                
+
             }
 
             serverFile serverObj = new serverFile()
             {
-                vault = svar4, 
+                vault = svar4,
                 IV = openIv //skapa en IV genom aes
             };
 
             string jsonString1 = JsonSerializer.Serialize(serverObj);
-            File.WriteAllText(@"ServerInfo2.json", jsonString1);
+            File.WriteAllText(@"IV.json", jsonString1);
         }
 
         public static byte[] readFromServerIv()
         {
-            string jsonString = File.ReadAllText(@"ServerInfo2.json");
+            string jsonString = File.ReadAllText(@"IV.json");
             serverFile readResult = JsonSerializer.Deserialize<serverFile>(jsonString);
 
             return readResult.IV;
@@ -77,7 +83,7 @@ namespace Code_off
 
         public static byte[] readFromServerVault()
         {
-            string jsonString = File.ReadAllText(@"ServerInfo2.json");
+            string jsonString = File.ReadAllText(@"IV.json");
             serverFile readResult = JsonSerializer.Deserialize<serverFile>(jsonString);
 
             return readResult.vault;
