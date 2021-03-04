831263523585dadc3fbf1cfe6333284a445f3a5a
nilsner <79632260+nilsner@users.noreply.github.com>
2021-03-04 14:17:29 +0100

    Update ConnectionKey.cs

--- ConnectionKey.cs
+++ ConnectionKey.cs
@@ -1,37 +1,56 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.IO;
 using System.Security.Cryptography;
 
-namespace PasswordManager
+namespace Code_off
 {
     public class ConnectionKey : SecretKey
     {
-        // possibly unnecessary
+        // pssibly unnecessary
         private const string usageText = "Usage: RFC2898 <password>\nYou must specify the password. \n";
-        public byte[] k1; // nytt
+        public static byte[] k1;
 
-        public byte[] ConnectsKeyAndPsw(string psw1)
+
+        public static byte[] ConnectsKeyAndPsw(string psw1)
         {
-            
-            byte[] salt1 = SKeyGenerator();
-          
+
+            byte[] salt1 = FileConnector.ReadFromFile();
+            //FileConnector.OverwriteClientPass(salt1); //SALT SPARAS NU I CLIENT FILEN
             try
             {
-                Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(psw1, salt1);
+                Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(psw1, salt1); //VART FRÖSVINNER SALT1
 
                 k1 = key.GetBytes(16); //nytt
+                
                 return k1;
             }
             catch (Exception e)
             {
                 Console.WriteLine("Error: {0}", e);
             }
-             return k1;
-            
+            return k1;
+
+
         }
 
-        
+        public byte[] RecreateRFC(string psw1)
+        {
+            byte[] salt1 = FileConnector.ReadFromFile();
+            try
+            {
+                Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(psw1, salt1);
+
+                k1 = key.GetBytes(16); //nytt
+
+                return k1;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: {0}", e);
+            }
+            return k1;
+        }
     }
 }
