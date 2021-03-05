400f3b2a1dfdf2a92a2aeff54c31014993f4198d
nilsner <79632260+nilsner@users.noreply.github.com>
2021-03-05 16:02:02 +0100

    Update Program.cs
    Felhantering och inlogg

--- Program.cs
+++ Program.cs
@@ -1,8 +1,4 @@
 using System;
-using System.IO;
-using System.Security.Cryptography;
-using System.Text.Json;
-using System.Text.Json.Serialization;
 
 namespace Code_off
 {
@@ -10,21 +6,29 @@ namespace Code_off
     {
         public static void Main()
         {
-
-            //FileConnectServer.CreateAccount(); //DEN HÄR ÄR REDAN KÖRD EN GÅNG OCH SPARAD, BEHÖVER ALDRIG GÖRAS OM IGEN. OM DETTA SKER KOMMER DECRYPTEN FACKAS
-            //string input2 = Console.ReadLine();
-            //string input3 = Console.ReadLine();
-            //FileConnectServer.DisplayVault("Nisse"); //Funkar
-            //FileConnectServer test = new FileConnectServer();
-            //ConnectionKey go = new ConnectionKey();
+            //ServerFileConnect.CreateAccount(); //DEN HÄR ÄR REDAN KÖRD EN GÅNG OCH SPARAD, BEHÖVER ALDRIG GÖRAS OM IGEN. OM DETTA SKER KOMMER DECRYPTEN FACKAS
             
-            //FileConnectServer.writeToServer("Nisse", input2, input3); //tar emot två saker, lösenord och text som ska sparas, det sista av de två är de som sparas i filen. Dvs Nisse är löseordet och det enda som finns sparat i vault atm är King
-            //string svar = AES.DecryptStringFromBytes_Aes(FileConnectServer.readFromServerVault(), ConnectionKey.ConnectsKeyAndPsw("Nisse"), FileConnectServer.readFromServerIv()); //hej ska vara samma console.readline som rad 17 representerar
-            //Console.WriteLine(svar);
-            FileConnectServer.DisplayVault("Nisse"); //Funkar
-            string RemoveKey = Console.ReadLine(); //FUNKAR INTE, MÅSTE SPARA NER PÅ JSON EFTER ATT HA UPPDATERAT DIC
-            Vault.RemoveFromVault(RemoveKey, "Nisse");
-            FileConnectServer.DisplayVault("Nisse");
+            string MasterPassword = "Nisse"; //Rätta koden är Nisse
+
+            if (ServerFileConnect.ControllPass(MasterPassword) == false)
+            {
+                //Lösenordet är fel
+            }
+            else
+            {
+                string input2 = Console.ReadLine();
+                string input3 = Console.ReadLine();
+                //ServerFileConnect.DisplayVault("Nisse"); //Display
+
+                ServerFileConnect.WriteToServer(MasterPassword, input2, input3); //tar emot två saker, lösenord och text som ska sparas, det sista av de två är de som sparas i filen. Dvs Nisse är löseordet och det enda som finns sparat i vault atm är King
+                                                                                 //string svar = AES.DecryptStringFromBytes_Aes(ServerFileConnect.ReadFromServerVault(), ConnectionKey.ConnectsKeyAndPsw("Nisse"), ServerFileConnect.ReadFromServerIv()); //hej ska vara samma console.readline som rad 17 representerar
+                                                                                 //Console.WriteLine(svar);
+                ServerFileConnect.DisplayVault(MasterPassword); //Funkar
+                string RemoveKey = Console.ReadLine(); //FUNKAR INTE, MÅSTE SPARA NER PÅ JSON EFTER ATT HA UPPDATERAT DIC
+                Vault.RemoveFromVault(RemoveKey, MasterPassword);
+                ServerFileConnect.DisplayVault(MasterPassword);
+            }
+            
         }
     }
 }
