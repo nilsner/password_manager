798bc0c5c80a08a70a1f4e31967824a33afc2037
ericremneback <79632735+ericremneback@users.noreply.github.com>
2021-03-15 15:21:55 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace PasswordManager3._0
             // bandy "client.json", "server.json"
             // fotboll "cl.json", "se.json"
 
-            args = new string[] { "get", "cl.json", "se.json" };
+            args = new string[] { "get", "client.json", "server.json" };
            
             int count = 0;
             if (args[0] == "secret")
@@ -82,22 +82,29 @@ namespace PasswordManager3._0
                     case "get": //HELT KLAR
                         Console.WriteLine("Type your master password");
                         string masterPassword = Console.ReadLine();
+                        string sKey = SecretKey.DisplaySecretKey(args[1]);                       
                         string prop = "";
                         string secretKeyGet = "";
-                        if (args.Length == 4)
+
+                        if (ServerFileConnect.ControllPass(masterPassword, args[1], args[2], sKey) == true)
                         {
-                            prop = args[3];
-                            ServerFileConnect.DisplayVault(masterPassword, args[1], args[2], prop, secretKeyGet);
-                        }
-                        else if (args.Length == 3)
-                        {
-                            ServerFileConnect.DisplayVault(masterPassword, args[1], args[2], prop, secretKeyGet);
-                        }
-                        else
-                        {
-                            Console.WriteLine("Something went wrong, try again");
-                        }
+                            if (args.Length == 4)
+                            {
+                                prop = args[3];
+                                ServerFileConnect.DisplayVault(masterPassword, args[1], args[2], prop, secretKeyGet);
+                            }
+                            else if (args.Length == 3)
+                            {
+                                ServerFileConnect.DisplayVault(masterPassword, args[1], args[2], prop, secretKeyGet);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Something went wrong, try again");
+                            }
+                        }   
                         break;
+
+                        
 
                     case "set": // Helt klar
                         if (args.Length > 4)
@@ -108,12 +115,17 @@ namespace PasswordManager3._0
                         {
                             Console.WriteLine("Type your master password");
                             string masterPasswordSet = Console.ReadLine();
-                            
-                            Console.WriteLine("Type the new password");
-                            string NewPas = Console.ReadLine();
-                            string secretKeySet = "";
+                            string secretKey = SecretKey.DisplaySecretKey(args[1]);
+                            if (ServerFileConnect.ControllPass(masterPasswordSet, args[1], args[2], secretKey) == true)
+                            {
+                                Console.WriteLine("Type the new password");
+                                string NewPas = Console.ReadLine();
+                                string secretKeySet = "";
 
-                            Vault.ChangeServicePsw(args[3], NewPas, masterPasswordSet, args[2], args[1], secretKeySet);
+                                Vault.ChangeServicePsw(args[3], NewPas, masterPasswordSet, args[2], args[1], secretKeySet);
+                            }
+
+                           
                         }
                         break;
 
@@ -126,10 +138,12 @@ namespace PasswordManager3._0
                         {
                             Console.WriteLine("Type your master password");
                             string masterPasswordDrop = Console.ReadLine();
-                            //Console.WriteLine("Type the name of the service you wish to delete");
-                            //string RemoveKey = Console.ReadLine();
-                            string secretKeyDrop = "";
-                            Vault.RemoveFromVault(args[3], masterPasswordDrop, args[2], args[1], secretKeyDrop);
+                            string secretKey = SecretKey.DisplaySecretKey(args[1]);
+                            if (ServerFileConnect.ControllPass(masterPasswordDrop, args[1], args[2], secretKey) == true)
+                            {                                     
+                                string secretKeyDrop = "";
+                                Vault.RemoveFromVault(args[3], masterPasswordDrop, args[2], args[1], secretKeyDrop);
+                            }    
                         }
                         break;
 
@@ -146,13 +160,17 @@ namespace PasswordManager3._0
                         {
                             Console.WriteLine("Type your master password");
                             string masterPasswordCreate = Console.ReadLine();
-                            Console.WriteLine("Type your new service:");
-                            string NewService = Console.ReadLine();
-                            Console.WriteLine("Type your password for " + NewService + ":");
-                            string NewPassword = Console.ReadLine();
-                            string secretKeyCreate = "";
-                            ServerFileConnect.WriteToServer(masterPasswordCreate, NewService, NewPassword, args[2], args[1], secretKeyCreate);
-                            Console.WriteLine("New service is created.");
+                            string secretKey = SecretKey.DisplaySecretKey(args[1]);
+                            if (ServerFileConnect.ControllPass(masterPasswordCreate, args[1], args[2], secretKey) == true)
+                            {
+                                Console.WriteLine("Type your new service:");
+                                string NewService = Console.ReadLine();
+                                Console.WriteLine("Type your password for " + NewService + ":");
+                                string NewPassword = Console.ReadLine();
+                                string secretKeyCreate = "";
+                                ServerFileConnect.WriteToServer(masterPasswordCreate, NewService, NewPassword, args[2], args[1], secretKeyCreate);
+                                Console.WriteLine("New service is created.");
+                            }
                         }
                         break;
 
