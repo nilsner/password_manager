475cf1d180cecb548b71e22d5a6a0cdfc708689b
nilsner <79632260+nilsner@users.noreply.github.com>
2021-03-23 12:47:06 +0100

    Update AES.cs

--- AES.cs
+++ AES.cs
@@ -51,7 +51,7 @@ namespace PasswordManager3._0
 
             if (cipherText == null || cipherText.Length <= 0)
                 throw new ArgumentNullException("cipherText");
-            if (Key == null || Key.Length <= 0 || Key.Length > 16) //Om längre än 16 så ska det inte köras
+            if (Key == null || Key.Length <= 0 || Key.Length > 16) 
                 throw new ArgumentNullException("Key");
             if (IV == null || IV.Length <= 0)
                 throw new ArgumentNullException("IV");
@@ -64,18 +64,17 @@ namespace PasswordManager3._0
                 aesAlg.Key = Key;
                 aesAlg.IV = IV;
 
-                // Create a decryptor to perform the stream transform.
+               
                 ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
 
-                // Create the streams used for decryption.
+                
                 using (MemoryStream msDecrypt = new MemoryStream(cipherText))
                 {
                     using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
                     {
                         using (StreamReader srDecrypt = new StreamReader(csDecrypt))
                         {
-                            // Read the decrypted bytes from the decrypting stream
-                            // and place them in a string.
+                            
                             try
                             {
                                 plaintext = srDecrypt.ReadToEnd();
