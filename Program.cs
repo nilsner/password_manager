1d78e4999eb15d017b3ec70273a1637b2624ad7d
nilsner <79632260+nilsner@users.noreply.github.com>
2021-04-15 10:49:45 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,13 +1,15 @@
-﻿using System;
+using System;
 using System.IO;
 
 namespace PasswordManager3._0
 {
     class Program
     {
-        public static void Main(string[] args)
+        public static void Main(string[] args) //string[] args
         {
-                       
+            //string test = "get nisseclient nisseserver"; 
+            //var args = test.Split(' ');
+
             int count = 0;
             if (args[0] == "secret")
             {
@@ -38,17 +40,17 @@ namespace PasswordManager3._0
             }
             else
             {
-                switch (args[0]) 
+                switch (args[0])
                 {
 
-                    case "login": 
+                    case "login": //OSÄKER HÄR
                         Console.WriteLine("Type your master password");
                         string masterPasswordLogin = Console.ReadLine();
                         Console.WriteLine("Type your secret key");
                         string secretKeyLogin = Console.ReadLine();
                         if (ServerFileConnect.ControllPass(masterPasswordLogin, args[1], args[2], secretKeyLogin) == true)
                         {
-                           Console.WriteLine("You're now logged in");
+                            Console.WriteLine("You're now logged in");
                         }
                         else
                         {
@@ -56,7 +58,7 @@ namespace PasswordManager3._0
                         }
                         break;
 
-                    case "init": 
+                    case "init": //FUNKAR APRIL
                         if (args.Length > 3)
                         {
                             Console.WriteLine("Input was given in wrong format");
@@ -70,10 +72,10 @@ namespace PasswordManager3._0
                         }
                         break;
 
-                    case "get": 
+                    case "get": //FUNKAR APRIL
                         Console.WriteLine("Type your master password");
                         string masterPassword = Console.ReadLine();
-                        string sKey = SecretKey.DisplaySecretKey(args[1]);                       
+                        string sKey = SecretKey.DisplaySecretKey(args[1]);
                         string prop = "";
                         string secretKeyGet = "";
 
@@ -92,12 +94,12 @@ namespace PasswordManager3._0
                             {
                                 Console.WriteLine("Something went wrong, try again");
                             }
-                        }   
+                        }
                         break;
 
-                        
 
-                    case "set": 
+
+                    case "set": //FUNKAR APRIL
                         if (args.Length > 4)
                         {
                             Console.WriteLine("Something went wrong, try again");
@@ -112,15 +114,14 @@ namespace PasswordManager3._0
                                 Console.WriteLine("Type the new password");
                                 string NewPas = Console.ReadLine();
                                 string secretKeySet = "";
-
                                 Vault.ChangeServicePsw(args[3], NewPas, masterPasswordSet, args[2], args[1], secretKeySet);
                             }
 
-                           
+
                         }
                         break;
 
-                    case "drop": 
+                    case "drop": //FUNKAR APRIL
                         if (args.Length > 4)
                         {
                             Console.WriteLine("Something went wrong, try again");
@@ -131,38 +132,15 @@ namespace PasswordManager3._0
                             string masterPasswordDrop = Console.ReadLine();
                             string secretKey = SecretKey.DisplaySecretKey(args[1]);
                             if (ServerFileConnect.ControllPass(masterPasswordDrop, args[1], args[2], secretKey) == true)
-                            {                                     
+                            {
                                 string secretKeyDrop = "";
                                 Vault.RemoveFromVault(args[3], masterPasswordDrop, args[2], args[1], secretKeyDrop);
-                            }    
-                        }
-                        break;
-
-                    case "secret": 
-                        Console.WriteLine("Your secret key is: \n" + SecretKey.DisplaySecretKey(args[1]));
-                        break;
-
-                    case "create": 
-                        if(args.Length != 3)
-                        {
-                            Console.WriteLine("Something went wrong, try again");
-                        }
-                        else
-                        {
-                            Console.WriteLine("Type your master password");
-                            string masterPasswordCreate = Console.ReadLine();
-                            string secretKey = SecretKey.DisplaySecretKey(args[1]);
-                            if (ServerFileConnect.ControllPass(masterPasswordCreate, args[1], args[2], secretKey) == true)
-                            {
-                                Console.WriteLine("Type your new service:");
-                                string NewService = Console.ReadLine();
-                                Console.WriteLine("Type your password for " + NewService + ":");
-                                string NewPassword = Console.ReadLine();
-                                string secretKeyCreate = "";
-                                ServerFileConnect.WriteToServer(masterPasswordCreate, NewService, NewPassword, args[2], args[1], secretKeyCreate);
-                                Console.WriteLine("New service is created.");
                             }
                         }
+                        break;
+
+                    case "secret": //FUNKAR APRIL
+                        Console.WriteLine("Your secret key is: \n" + SecretKey.DisplaySecretKey(args[1]));
                         break;
 
                     default:
